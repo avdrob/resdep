5ce4959571915bb0e70693dab984a870d491156b
Andrey Drobyshev <drmtmych@virtuozzo.mipt.ru>
2018-04-12 23:27:27 +0300

    Derive sleep/work time instead of global constants

--- kmod/kcpuhog.c
+++ kmod/kcpuhog.c
@@ -9,37 +9,43 @@
 #include <linux/sched/types.h>
 #endif
 
-#define MS_TO_NS(x) (x * 1E6L)
-#define MS_TO_US(x) (x * 1E3L)
+#define MSEC_IN_SEC	1000
+#define MS_TO_NS(x)	(x * 1000000)
+#define MS_TO_US(x)	(x * 1000)
 
 struct hog_thread_data {
 	struct task_struct *hog_thread;
 	struct hrtimer hog_hrtimer;
 	int cpu;
+	unsigned long work_time_ms;
+	unsigned long sleep_time_ms;
 	bool is_running;
 };
 static struct hog_thread_data hog_data[4];
 
 // static struct task_struct *hog_thread;
 // static struct hrtimer hog_hrtimer;
-static bool is_running;
+// static bool is_running;
 
-const unsigned long work_time_ms = 200L;
-const unsigned long sleep_time_ms = 800L;
+// unsigned long work_time_ms = 200;
+// unsigned long sleep_time_ms = 800;
 
-enum hrtimer_restart hog_hrtimer_callback(struct hrtimer *timer )
+enum hrtimer_restart hog_hrtimer_callback(struct hrtimer *timer)
 {
+	struct hog_thread_data *data = container_of(timer,
+				struct hog_thread_data, hog_hrtimer);
+
 	printk(KERN_INFO "[kcpuhog]: hrtimer_callback is called\n");
 
-	if (is_running) {
-		is_running = false;
+	if (data->is_running) {
+		data->is_running = false;
 		hrtimer_forward_now(timer,
-				ktime_set(0, MS_TO_NS(sleep_time_ms)));
+				ktime_set(0, MS_TO_NS(data->sleep_time_ms)));
 	}
 	else {
-		is_running = true;
+		data->is_running = true;
 		hrtimer_forward_now(timer,
-				ktime_set(0, MS_TO_NS(work_time_ms)));
+				ktime_set(0, MS_TO_NS(data->work_time_ms)));
 	}
 
 	return HRTIMER_RESTART;
@@ -51,18 +57,19 @@ static int hog_threadfn(void *d)
 
 	hrtimer_init(&data->hog_hrtimer, CLOCK_MONOTONIC, HRTIMER_MODE_REL);
 	data->hog_hrtimer.function = hog_hrtimer_callback;
-	hrtimer_start(&data->hog_hrtimer, ktime_set(0, MS_TO_NS(work_time_ms)),
-			HRTIMER_MODE_REL);
+	hrtimer_start(&data->hog_hrtimer, ktime_set(0,
+				MS_TO_NS(data->work_time_ms)), HRTIMER_MODE_REL);
 
-	is_running = true;
+	data->is_running = true;
 	while (!kthread_should_stop()) {
 		printk(KERN_INFO "[kcpuhog]: begin busyloop\n");
-		while (is_running)
+		while (data->is_running)
 			cpu_relax();
 		printk(KERN_INFO "[kcpuhog]: end busyloop\n");
 
 		printk(KERN_INFO "[kcpuhog]: begin sleep\n");
-		usleep_range(MS_TO_US(sleep_time_ms), MS_TO_US(sleep_time_ms));
+		usleep_range(MS_TO_US(data->sleep_time_ms),
+				MS_TO_US(data->sleep_time_ms));
 		printk(KERN_INFO "[kcpuhog]: end sleep\n");
 	}
 
@@ -81,7 +88,9 @@ static int __init kcpuhog_init(void)
 	printk(KERN_INFO "[kcpuhog]: Creating Thread\n");
 	hog_data[0].cpu = 1;
 	hog_data[0].hog_thread = kthread_create(hog_threadfn, &(hog_data[0]),
-			"kcpuhog_%d", hog_data[0].cpu);
+				"kcpuhog_%d", hog_data[0].cpu);
+	hog_data[0].work_time_ms = 270;
+	hog_data[0].sleep_time_ms = MSEC_IN_SEC - hog_data[0].work_time_ms;
 	kthread_bind(hog_data[0].hog_thread, hog_data[0].cpu);
 	wake_up_process(hog_data[0].hog_thread);
 
